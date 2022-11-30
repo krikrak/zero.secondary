82848dc410a6ad90a3cd76bc3d4bd79270905ec5
krikrak <hello@krikrak.fr>
2022-11-30 15:51:38 +0100

    Clean Basechild

--- functions.php
+++ functions.php
@@ -1,14 +1,30 @@
 <?php
+                                                                                    
+//    . .       . .       . .                 . .       . .       . .    
+// .+'|=|`+. .+'| |`+. .+'|=|`+. |~~|=|~~| .+'|=|`+. .+'|=|`+. .+'|=|`+. 
+// |  | `+.| |  | |  | |  | |  | |.+' |  | |  | `+.| |  | |  | |  | |  | 
+// |  | .    |  | |  | |  |'. '.      |.+' |  |=|`.  |  |'. '. |  | |  | 
+// `+.|=|`+. |  | |  | |  | |  |    .='    |  | `.|  |  | |  | |  | |  | 
+// .    |  | |  | |  | |  | |  | .+'|    . |  |    . |  | |  | |  | |  | 
+// |`+. |  | |  | |  | |  | |  | |  | .+'| |  | .+'| |  | |  | |  | |  | 
+// `+.|=|.+' `+.|=|.+' `+.|=|.+' |..|=|..| `+.|=|.+' `+.| |.+' `+.|=|.+' 
+                                                                      
+/* Overload zero_function */                                                                              
 
-function enqueue_styles()
-{
-    // $parent_style = 'zero-style';
-    // wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style(
-        'children_name-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        '',
-        wp_get_theme()->get('Version')
-    );
-}
-// add_action('wp_enqueue_scripts', 'enqueue_styles');
+    /* Default stylesheet added by zero */
+    /* Parent = default stylesheet -> source in zero */
+    // function enqueue_styles()
+    // {
+    //     // $parent_style = 'zero-style';
+    //     // wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    //     wp_enqueue_style(
+    //         'children_name-style',
+    //         get_stylesheet_directory_uri() . '/style.css',
+    //         '',
+    //         wp_get_theme()->get('Version')
+    //     );
+    // }
+    // // add_action('wp_enqueue_scripts', 'enqueue_styles');
+       
+                                  
+
