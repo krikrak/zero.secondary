5fd804ee37d7e66ecadfdb1dc299a093b395e71d
krikrak <hello@krikrak.fr>
2022-12-13 09:58:07 +0100

    Update project for zero->develop

---
+++ patterns/post-grid_item.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Post Meta
+ * Slug: twentytwentythree/post-meta
+ * Categories: query
+ * Keywords: post meta
+ * Block Types: core/template-part/post-meta
+ */
+?>
+<!-- wp:spacer {"height":"0"} -->
+<div style="height:0" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
